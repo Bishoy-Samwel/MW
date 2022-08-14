5208695fafa990d5a52d7ba5cc7bcb29067f3ece
Bishoy <bishoy.samwuel@gmail.com>
2022-08-14 13:50:02 +0200

    Get photos from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+import { getAll } from "./api";
+import { useEffect, useState } from 'react';
+import { ImageSlider } from './components/ImageSlider';
 function App() {
-  const [count, setCount] = useState(0)
+  const [photos, setPhotos] = useState([]);
+
+  useEffect( () => {
+    getAll().then((data) => {console.log(data);})
+  },[])
+
+  const slides =  [
+    {url: "http://127.0.0.1:5173/assets/1.jpg"},
+    {url: "http://127.0.0.1:5173/assets/2.jpg"},
+    {url: "http://127.0.0.1:5173/assets/3.jpg"},
+    {url: "http://127.0.0.1:5173/assets/4.jpg"},
+    {url: "http://127.0.0.1:5173/assets/5.jpg"}
+  ]
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App" style ={containerStyles}>
+      <ImageSlider slides={slides} />
     </div>
   )
 }
