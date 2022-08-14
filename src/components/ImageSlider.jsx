399faf6a1c86c4eb3e2749717cb267ed4ba154f2
Bishoy <bishoy.samwuel@gmail.com>
2022-08-14 16:29:03 +0200

    Add next and previous functionality?

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export const ImageSlider = ({ slides }) => {
-  const [currentIndex, setCurrentUser] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
   const slideStyles = {
     width: "100%",
     height: "100%",
@@ -31,11 +31,15 @@ export const ImageSlider = ({ slides }) => {
   }
 
   const goToPrevious = () => {
-  
+    const isFirstSlide = currentIndex ===0;
+    const newIndex = isFirstSlide? slides.length - 1: currentIndex - 1;
+    setCurrentIndex(newIndex);
   }
   
   const goToNext = () => {
-
+    const isLastSlide = currentIndex === slides.length -1;
+    const newIndex = isLastSlide? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
   }
 
   return (
