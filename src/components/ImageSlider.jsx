4d45093bf3301de5eb9db65088d49194be338893
Bishoy <bishoy.samwuel@gmail.com>
2022-08-14 18:44:39 +0200

    Add go to slide functionality

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -14,7 +14,7 @@ export const ImageSlider = ({ slides }) => {
   const sliderStyles = {
     height: "100%",
     display: "flex",
-    alignItems:"center",
+    alignItems: "center",
     justifyItems: "spaceBetween"
   }
 
@@ -31,22 +31,47 @@ export const ImageSlider = ({ slides }) => {
   }
 
   const goToPrevious = () => {
-    const isFirstSlide = currentIndex ===0;
-    const newIndex = isFirstSlide? slides.length - 1: currentIndex - 1;
-    setCurrentIndex(newIndex);
-  }
-  
-  const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length -1;
-    const newIndex = isLastSlide? 0 : currentIndex + 1;
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   }
 
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex)
+  }
+
+  const topIconsContainer = {
+    display: "flex",
+    justifyContent: "center"
+  }
+
+  const topIconStyles = {
+    margin: "0 3px",
+    cursor: "pointer",
+    fontSize: "20px"
+  }
   return (
-    <div style={sliderStyles}>
-      <div style={leftArrowStyles} onClick={goToPrevious}>O</div>
-      <div style={slideStyles}></div>
-      <div style={rightArrowStyles} onClick={goToNext}>O</div>
-    </div>
+    <>
+      <div style={topIconsContainer} >
+        {
+          slides.map((slide, slideIndex) => (
+            <div style={topIconStyles} onClick={() => goToSlide(slideIndex)} key={slideIndex}>
+              {slide.title}
+            </div>
+          ))
+        }
+      </div>
+      <div style={sliderStyles}>
+        <div style={leftArrowStyles} onClick={goToPrevious}>O</div>
+        <div style={slideStyles}></div>
+        <div style={rightArrowStyles} onClick={goToNext}>O</div>
+      </div>
+    </>
   )
 }
