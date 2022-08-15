99d0869d3fad60254a72d511d68221842dc5df1f
Bishoy <bishoy.samwuel@gmail.com>
2022-08-16 01:22:47 +0200

    Remove styles from JSX file

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,60 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiArrowLeftCircle } from "react-icons/fi";
 
 export const Slider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const slideStyles = {
-    width: "100%",
-    height: "100%",
-    borderRadious: "10px",
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundImage: `url(${slides[currentIndex].url})`,
-  }
 
-  const sliderStyles = {
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyItems: "spaceBetween"
-  }
-
-  const leftArrowStyles = {
-    cursor: "pointer",
-    color: "#999",
-    border: "2px solid #999",
-    width: "25px",
-    height: "25px",
-    borderRadius: "50%",
-  }
-
-  const rightArrowStyles = {
-    cursor: "pointer",
-    color: "#999",
-    border: "2px solid #999",
-    width: "25px",
-    height: "25px",
-    borderRadius: "50%",
-  }
-
-  const topIconsContainerStyles = {
-    width: "50px",
-    height: "500px",
-    overflowY: "auto",
-    overflowX: "hidden",
-    transform: "rotate(-90deg)",
-    transformOrigin: "right top",
-    transform:"rotate(-90deg) translateY(-100px)"
-  }
-
-  const topIconStyles = {
-    transform: "rotate(90deg)",
-    transformOrigin: "right top",
-    margin: "10px",
-    cursor: "pointer",
-    fontSize: "20px"
-  }
+  // useEffect(() => {
+  //   document.documentElement.style.setProperty('imageUrl',slides[currentIndex].url)
+  // })
 
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
@@ -69,25 +22,26 @@ export const Slider = ({ slides }) => {
   }
 
   const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex-1)
+    setCurrentIndex(slideIndex - 1)
   }
 
 
   return (
     <>
-      <div style={topIconsContainerStyles} >
+      <div className="topIconsContainer" >
         {
           slides.map((slide) => (
-            <div style={topIconStyles} onClick={() => goToSlide(slide.id)} key={slide.id}>
-              {slide.id}
+            <div className="topIcons" 
+            style={{backgroundImage: `url(${slide.thumbnailUrl})`}}
+            onClick={() => goToSlide(slide.id)} key={slide.id}>
             </div>
           ))
         }
       </div>
-      <div style={sliderStyles}>
-        <div style={leftArrowStyles} onClick={goToPrevious}><i className="arrow left"></i></div>
-        <div style={slideStyles}></div>
-        <div style={rightArrowStyles} onClick={goToNext}><i className="arrow right"></i></div>
+      <div className="slider">
+        <div className="arrow" onClick={goToPrevious}>&lt;</div>
+        <div className="slides" style={{backgroundImage: `url(${slides[currentIndex].url})`}}></div>
+        <div className="arrow" onClick={goToNext}>&gt;</div>
       </div>
     </>
   )
